fe7437e5cda01d21bb350f08c22711c6cac33e4d
Ronz97 <rmdelgado@uta.cv>
2021-12-14 13:22:04 -0100

    Ligacao das tela login e cadastro Intent

--- app/src/main/java/com/example/projecto/FormLogin.java
+++ app/src/main/java/com/example/projecto/FormLogin.java
@@ -2,14 +2,30 @@ package com.example.projecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+     private TextView text_TelaCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
         getSupportActionBar().hide();
+        IniciasComponetes();
+
+        text_TelaCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FormLogin.this,FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void IniciasComponetes(){
+        text_TelaCadastro = findViewById(R.id.text_Telacadastrar);
     }
 }
